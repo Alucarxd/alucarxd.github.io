5781ab155c764a34645d66699af62de6ae1e8c92
Gabriel Garcia <58672542+Alucarxd@users.noreply.github.com>
2021-02-24 18:24:39 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# alucarxd.github.io
+Sitio personal
+
+# Utilidades
+
+ - 1. Instalar lo que necesitamos
+ **pip install pelican markdown invoke liverload**
+-  2. Iniciar asistente de pelican
+ **pelican-quickstart**
+Seguido a eso insertamos
+
+invoke build **generar el sitio**
---
+++ publishconf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://alucarxd.github.io'
+RELATIVE_URLS = False
+
+GOOGLE_ANALYTICS = "G-8B8DH6X6XL"
+DISQUS_SITENAME = "https-alucarxd-github-io"
+
+RECORD_SESSION = True
+
+DELETE_OUTPUT_DIRECTORY = True
